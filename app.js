e2d9a8c1a2752206201eaa7dabd2e29c6be2ee6d
Ricky Boyd <rickyboyd23@gmail.com>
2021-11-08 07:36:29 -0500

    further structured backend setup

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import logger from 'morgan'
+
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use(express.json())
+app.use(logger('dev'))
+
+app.use('/post', postRoutes);
+
+export default app
+
+// May need to come back and change 'post' on line 11 based on the syntax
