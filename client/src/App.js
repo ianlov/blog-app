e0acd714a2b7b7950ac31559dacaabe67ece20c4
Vince Abuyuan <rpabuyuan@ucsb.edu>
2021-11-09 13:54:05 -0800

    post

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import Home from "./screens/Home/Home";
+import Post from "./screens/Post/Post";
+
 function App() {
   return (
     <div className="App">
       <Home />
+      <Post />
     </div>
   );
 }
