3e63e5d5958082134039696bf047772155e25863
Vince Abuyuan <rpabuyuan@ucsb.edu>
2021-11-09 12:27:59 -0800

    stuff

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-import express from 'express'
-import logger from 'morgan'
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
 
-import postRoutes from './routes/posts.js'
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use(express.json())
-app.use(logger('dev'))
+app.use(express.json());
+app.use(cors());
+app.use(logger("dev"));
 
-app.use('/api', postRoutes);
+app.use("/api", postRoutes);
 
-export default app
+export default app;
 
 // May need to come back and change 'post' on line 11 based on the syntax
